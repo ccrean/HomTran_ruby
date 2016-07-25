94159796dfd0fa2ebc2db542f00629f28ba1ad44
Cory Crean <cory.crean@gmail.com>
2016-07-24 20:09:40 -0400

    Add initializer and getters

--- lib/homogeneous_transformation.rb
+++ lib/homogeneous_transformation.rb
@@ -1,5 +1,20 @@
-require "homogeneous_transformation/version"
+require_relative 'homogeneous_transformation/version'
+require 'unit_quaternion'
 
-module HomogeneousTransformation
-  # Your code goes here...
+class HomogeneousTransformation
+  def initialize(q = UnitQuaternion.new(1, 0, 0, 0),
+                 translation = Vector[0, 0, 0])
+    @q = q
+    @t = translation
+  end
+
+  def getQuaternion()
+    return @q
+  end
+
+  def getTranslation()
+    return @t
+  end
 end
+
+HomTran = HomogeneousTransformation
--- lib/homogeneous_transformation/version.rb
+++ lib/homogeneous_transformation/version.rb
@@ -1,3 +1,3 @@
-module HomogeneousTransformation
+class HomogeneousTransformation
   VERSION = "0.0.1"
 end
